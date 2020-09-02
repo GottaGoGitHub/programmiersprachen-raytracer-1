f7933514ef26f83acd054da349c7c17960ac3450
Jonas Roquette <roquettejonas@gmail.com>
2020-09-02 11:27:05 +0200

    Added light class

---
+++ framework/light.hpp
@@ -0,0 +1,18 @@
+#ifndef LIGHT_HPP
+#define LIGHT_HPP
+
+#include "color.hpp"
+#include <glm/vec3.hpp>
+
+struct Light {
+    std::string name = "Default Lamp";
+    glm::vec3 position{0.0f, 0.0f, 0.0f};
+    Color tone{255, 147, 41}; //Candle Light
+    float intensity = 10.0f;
+
+    //Constructor ??
+
+    
+};
+
+#endif //LIGHT_HPP
