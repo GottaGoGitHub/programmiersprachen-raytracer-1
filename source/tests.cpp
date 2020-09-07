c2889fdfed9c809d465842d7fd18ac783a1a9414
Jonas Roquette <roquettejonas@gmail.com>
2020-09-07 23:20:04 +0200

    tests updated

--- source/tests.cpp
+++ source/tests.cpp
@@ -15,7 +15,9 @@ TEST_CASE("sphere") {
     std::string name{ "Eric" };
     Color color{ 1.5, 0.75, 1.2 };
 
-    Sphere sphere{ name, color, center, radius };
+    Material blue{ 0, 0, 0.7, 0, 0, 0.1, 0, 0, 0.1, 10 };
+
+    Sphere sphere{name, center, radius, blue};
 
     REQUIRE(sphere.area() == Approx(113.09734f));
     REQUIRE(sphere.volume() == Approx(113.09451f));
@@ -38,45 +40,46 @@ TEST_CASE("box") {
 }
 */
 
-TEST_CASE("intersect_ray_sphere", "[intersect]")
-{
-    // Ray
-    glm::vec3 ray_origin{ 0.0f, 0.0f, 0.0f };
-    // ray direction has to be normalized !
-    // you can use :
-    // v = glm :: normalize ( some_vector )
-    glm::vec3 ray_direction{ 0.0f, 0.0f, 1.0f };
-    // Sphere
-    glm::vec3 sphere_center{ 0.0f ,0.0f, 5.0f };
-    float sphere_radius{ 1.0f };
-    float distance = 0.0f;
-    auto result = glm::intersectRaySphere(
-        ray_origin, ray_direction,
-        sphere_center,
-        sphere_radius * sphere_radius, // squared radius !!!
-        distance);
-    REQUIRE(distance == Approx(4.0f));
-}
+// TEST_CASE("intersect_ray_sphere", "[intersect]")
+// {
+//     // Ray
+//     glm::vec3 ray_origin{ 0.0f, 0.0f, 0.0f };
+//     // ray direction has to be normalized !
+//     // you can use :
+//     // v = glm :: normalize ( some_vector )
+//     glm::vec3 ray_direction{ 0.0f, 0.0f, 1.0f };
+//     // Sphere
+//     glm::vec3 sphere_center{ 0.0f ,0.0f, 5.0f };
+//     float sphere_radius{ 1.0f };
+//     float distance = 0.0f;
+//     auto result = glm::intersectRaySphere(
+//         ray_origin, ray_direction,
+//         sphere_center,
+//         sphere_radius * sphere_radius, // squared radius !!!
+//         distance);
+//     REQUIRE(distance == Approx(4.0f));
+// }
 
-TEST_CASE("task 5.8", "[virtual]"){
-    Color red{ 255 , 0 , 0 };
-    glm::vec3 position{ 0.0f, 0.0f, 0.0f };
-    Sphere* s1 = new Sphere{ "sphere0" , red ,position , 1.2f};
-    Shape* s2 = new Sphere{ "sphere1" , red, position , 1.2f };
-    s1->print(std::cout);
-    s2->print(std::cout);
-    delete s1;
-    delete s2;
-}
-/*
-entfernt virtual von der Basisklasse, wird beim zweiten aufruf nur der Shape Destruktor und nicht der Sphere Destruktor verwendet
-*/
+// TEST_CASE("task 5.8", "[virtual]"){
+//     Color red{ 255 , 0 , 0 };
+//     glm::vec3 position{ 0.0f, 0.0f, 0.0f };
+//     Material blue{ 0, 0, 0.7, 0, 0, 0.1, 0, 0, 0.1, 10 };
+//     Sphere* s1 = new Sphere{ "sphere0",position , 1.2f, blue};
+//     Shape* s2 = new Sphere{ "sphere1", position , 1.2f, blue };
+//     s1->print(std::cout);
+//     s2->print(std::cout);
+//     delete s1;
+//     delete s2;
+// }
+// /*
+// entfernt virtual von der Basisklasse, wird beim zweiten aufruf nur der Shape Destruktor und nicht der Sphere Destruktor verwendet
+// */
 
-TEST_CASE("testing mat", "[material]"){
-    Material m;
-    REQUIRE(m.name_ == "default material");
-    //std::cout << *def << std::endl;
-}
+// TEST_CASE("testing mat", "[material]"){
+//     Material m;
+//     REQUIRE(m.name_ == "default material");
+//     //std::cout << *def << std::endl;
+// }
 
 // TEST_CASE("sdfloader", "[sdf]"){
 //     std::string filepath = "../files/scene.txt";
