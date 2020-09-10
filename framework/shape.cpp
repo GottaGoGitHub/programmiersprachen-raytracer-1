5f63ea1f959889d702c867221cf270e4ef01d09c
fibe8602 <ericgrunitz@gmail.com>
2020-09-10 17:48:51 +0200

    added transformation to shape + translation function

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -1,5 +1,6 @@
 #include "shape.hpp"
 
+
 Shape::Shape():
 	name_{"NULL"},
 	material_{}{}
@@ -18,4 +19,30 @@ std::ostream& Shape::print(std::ostream& os) const {
 
 std::ostream& operator<<(std::ostream& os, Shape const& s) {
 	return s.print(os);
+}
+
+void Shape::translate(glm::vec3 const& axis) {
+
+	glm::mat4 translation_matrix = glm::mat4{
+
+		glm::vec4(1.0f, 0.0f, 0.0f, axis.x),
+		glm::vec4(0.0f, 1.0f, 0.0f, axis.y),
+		glm::vec4(0.0f, 0.0f, 1.0f, axis.z),
+		glm::vec4(0.0f, 0.0f, 0.0f, 1.0f)
+
+	};
+
+	world_transformation_ = translation_matrix * world_transformation_;
+	world_transformation_inv_ = glm::inverse(world_transformation_);
+
+}
+
+void Shape::scale(float phi, glm::vec3 const& axis) {
+
+}
+
+void Shape::rotate(float phi, glm::vec3 const& axis) {
+
+	phi = phi * M_PI / 180;
+
 }
--- framework/shape.hpp
+++ framework/shape.hpp
@@ -1,10 +1,13 @@
 #ifndef SHAPE_HPP
 #define SHAPE_HPP
+
+#define _USE_MATH_DEFINES
 #include "color.hpp"
 #include <iostream>
 #include "hitpoint.hpp"
 #include "ray.hpp"
 #include "material.hpp"
+#include <glm/mat4x4.hpp>
 class Shape {
 	public:
 		Shape();
@@ -14,10 +17,17 @@ class Shape {
 		virtual float volume(void) const  = 0 ;
 		virtual std::ostream& print(std::ostream& os) const;
 		virtual HitPoint intersect(Ray ray) = 0;
+
+		void rotate(float phi, glm::vec3 const& axis);
+		void scale(float scalar, glm::vec3 const& direction);
+		void translate(glm::vec3 const& direction);
+
 	protected:
 		std::string name_;
 		//Color color_;
 		Material material_;
+		glm::mat4x4 world_transformation_{};
+		glm::mat4x4 world_transformation_inv_{};
 };
 std::ostream& operator<<(std::ostream& os, Shape const& s);
 #endif
